a0fece4919fb06c21dc9ce7f37406eca73a19fdb
yethihaoo123 <58133225+yethihaoo123@users.noreply.github.com>
2022-07-13 12:00:16 +0630

    Add files via upload

---
+++ portfolio/js/script.js
@@ -0,0 +1,7 @@
+/* =================================== typing animation ===================================== */
+var typed = new Typed(".typing",{
+    strings:["", "Web Designer", "Web Developer", "App Creator", "Gamer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true 
+})
